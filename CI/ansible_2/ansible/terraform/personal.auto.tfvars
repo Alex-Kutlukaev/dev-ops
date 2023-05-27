9b20137c8d357c62272ffe5181cb95586f356896
Alex-Kutlukaev <alexkutlukaev@gmail.com>
2023-05-27 16:53:41 +0300

    add ci1

---
+++ CI/ansible_2/ansible/terraform/personal.auto.tfvars
@@ -0,0 +1,4 @@
+token  =  "y0_AgAAAAARrkq8AATuwQAAAADalvwSK1AuHxLGTPm459rQ5gZjDd0JYX4"
+cloud_id  = "b1gtob9reohhtoq182pj"
+folder_id = "b1g7ske3eoqq2v0bqnub"
+default_zone = "ru-central1-a"
---
+++ CI/ansible_2/ansible/terraform/variable.tf
@@ -0,0 +1,83 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC68tSv0b6MTqfC5/hFL7wHQNn+znkaVx9yqvBDwxirw5emd5b8btXSdHpT70xVMPky6ILAU81m3siyw3NKn6MIXTldsQPSOf2FD5wMPmlYOQQ7b2gnmhugD43ebAssZhMSE+jRrh09UNA0vJlI0+qa4nErIQWEzPx5X3WTcU2FuzElpTrwIf9vmeLgNRrqE+IyRtsZDRxYvsSvkWTi0ELyM7LbXV25yCEZ4pwQETTB/WSolI3GIEeO9IXx9bdCDJUyh0h6Zw5EpjtM4yDnLCid4SzWutoKVtgLA4IPceV0mDEw86GFa0TSwHSW9NM1GNRmxHaIbDcfMy5b+mo6Xh62qDEosSB4pl5lXGbQG3p1k1pHq1vU138vb7hNHbs/9t4XkfYuR6uuJkCh9hUk64lX5yk1+AkIM3lGolkdPYvF/cWIdVuZCaDJdmvR6Rh5eU1iVWK4NVSvkhQ8mUDrzCxvRUkznufLbwpUeab3CmYVORC3x27Ww1ib6HuJnTf7dac= alekseykutlukaev@MacBook-Pro-Aleksej.local"
+}
+
+
+variable "image_name" {
+  type = string
+  description = "centos"
+  default = "centos-7"
+}
+
+variable "instance_name" {
+  type = string
+  default = "clickhouse"
+}
+
+variable "instance_name1" {
+  type    = string
+  default = "vector"
+
+  }
+
+variable "vm_web_resources" {
+  type        = map
+  default     = {
+    cores         = "2"
+    memory        = "2"
+    core_fraction = "5"
+  }
+}
+variable "vm_db_resources" {
+  type    = map
+  default = {
+    cores         = "2"
+    memory        = "2"
+    core_fraction = "5"
+  }
+}
+
+variable "connect" {
+  type = map
+  default = {
+    serial-port-enable = "1"
+    ssh-keys = "centos:var.vms_ssh_root_key}"
+  }
+}
